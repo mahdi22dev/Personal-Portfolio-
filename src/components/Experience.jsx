5f85c453eec3288ba6816e1b4e6345c53cc58a61
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-09-28 13:09:44 +0100

    experince component

---
+++ src/components/Experience.jsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+import "react-vertical-timeline-component/style.min.css";
+import { useSectionInView } from "@/lib/hooks";
+import { experiencesData } from "@/config/data";
+import SectionHeading from "./SectionHeading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+export default function Experience() {
+  const { ref } = useSectionInView("Experience");
+  const theme = "lirgght";
+
+  return (
+    <section id='experience' ref={ref} className='scroll-mt-28 mb-28 sm:mb-40'>
+      <SectionHeading>My experience</SectionHeading>
+      <VerticalTimeline lineColor=''>
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              contentStyle={{
+                visibility: "visible",
+                zIndex: "2222",
+                background:
+                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+                boxShadow: "none",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{
+                borderRight:
+                  theme === "light"
+                    ? "0.4rem solid #9ca3af"
+                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
+                zIndex: "2222",
+              }}
+              date={item.date}
+              icon={item.icon}
+              iconStyle={{
+                background:
+                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                fontSize: "1.5rem",
+                zIndex: "2222",
+              }}
+            >
+              <h3 className='font-semibold capitalize'>{item.title}</h3>
+              <p className='font-normal !mt-0'>{item.location}</p>
+              <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>
+                {item.description}
+              </p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
+  );
+}
