35c5c5e3cedb56c5598dc0a0adddcdcc6e11517c
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-09-28 13:14:20 +0100

    remove icons

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -51,7 +51,6 @@ export default function Experience() {
               <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>
                 {item.description}
               </p>
-              {item.icon}
             </VerticalTimelineElement>
           </React.Fragment>
         ))}
