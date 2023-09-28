451bbae7054208ec0e7b0f98b3e5060c14165a11
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-09-28 02:49:21 +0100

    update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,8 @@
 "use client";
-import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
-import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 const Intro = () => {
@@ -12,45 +10,10 @@ const Intro = () => {
   return (
     <section
       id='home'
-      className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'
+      className='mb-44 max-w-[50rem] text-center sm:mb-0 scroll-mt-[130rem]'
       ref={ref}
     >
-      <div className='flex items-center justify-center'>
-        {/* <div className='relative'>
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
-            <Image
-              src='https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100'
-              alt='Ricardo portrait'
-              width='192'
-              height='192'
-              quality='95'
-              priority={true}
-              className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
-            />
-          </motion.div>
-
-          <motion.span
-            className='absolute bottom-0 right-0 text-4xl'
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
-            }}
-          >
-            👋
-          </motion.span>
-        </div> */}
-      </div>
+      <div className='relative h-24 w-24'></div>
 
       <motion.h1
         className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
