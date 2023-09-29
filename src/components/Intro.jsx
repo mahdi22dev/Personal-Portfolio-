73846861840730bc55ae01fdb4162e96c917e450
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-09-29 03:45:27 +0100

    test styling

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -21,7 +21,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className='font-bold'>Hello, I'm Mahdi.</span> I'm a{" "}
-        <span className='font-bold'>front-End developer</span> with{" "}
+        <span className='font-bold'>front-end developer</span> with{" "}
         <span className='font-bold'>1+ years</span> of experience. I enjoy
         building <span className='italic'>sites & apps</span>. My focus is{" "}
         <span className='underline'>React (Next.js)</span>.
