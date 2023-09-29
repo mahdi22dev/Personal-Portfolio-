5b6f8979b84c12a6fb6b4366f5434e6917982c84
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-09-29 01:40:05 +0100

    theme switch

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/context";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
+import ThemeSwitch from "@/components/ThemeSwitch";
+import ThemeContextProvider from "@/context/theme-switch";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -21,10 +23,13 @@ export default function RootLayout({ children }) {
         <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]'></div>
         <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]'></div>
         <ActiveSectionContextProvider>
-          <Toaster position='top-right' />
-          <Navbar />
-          {children}
-          <Footer />
+          <ThemeContextProvider>
+            <Toaster position='top-right' />
+            <ThemeSwitch />
+            <Navbar />
+            {children}
+            <Footer />
+          </ThemeContextProvider>
         </ActiveSectionContextProvider>
       </body>
     </html>
