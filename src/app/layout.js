447cd60e8b2a8315c3435aee9ec605b1b8150dcd
Mahdi idrissi <129465404+mahdi22dev@users.noreply.github.com>
2024-10-22 01:15:13 +0100

    add plausible

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import { Toaster } from "react-hot-toast";
 import ThemeSwitch from "@/components/ThemeSwitch";
 import ThemeContextProvider from "@/context/theme-switch";
 const inter = Inter({ subsets: ["latin"] });
+import Script from 'next/script'
 
 export const metadata = {
   title: "Mahdi | Personal Portfolio",
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-20 sm:pt-28 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
+        <Script defer data-domain="portfolio.creativehandles.net" src="http://plausible-x0ogg0s4s0csoogwc884wcsw.94.16.115.142.sslip.io/js/script.js"></Script>
         <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]'></div>
         <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]'></div>
         <ActiveSectionContextProvider>
